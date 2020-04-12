642d87754f1f3ea12e04cdeb635739bc53813258
Anderson Menezes <andersonhsantanappa@gmail.com>
2020-04-12 10:36:24 -0300

    implementacao layout

--- src/app/features/panel/accounts/accounts.module.ts
+++ src/app/features/panel/accounts/accounts.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatRippleModule } from '@angular/material/core';
+
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { AccountsViewComponent } from './accounts-view/accounts-view.component';
 import { AccountsComponent } from './accounts.component';
 
 const COMPONENTS = [AccountsViewComponent, AccountsComponent];
+const MATERIAL_IMPORTS = [MatButtonModule, MatDividerModule, MatRippleModule];
 
 @NgModule({
   declarations: [COMPONENTS],
-  imports: [CommonModule, AccountsRoutingModule],
+  imports: [CommonModule, AccountsRoutingModule, MATERIAL_IMPORTS],
 })
 export class AccountsModule {}
