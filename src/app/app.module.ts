cf274654e6dafb7583a35c384f68738f7562d0f2
Anderson Menezes <andersonhsantanappa@gmail.com>
2020-05-10 10:11:53 -0300

    rename features to pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FeaturesRoutingModule } from "./features/features-routing.module";
-import { FeaturesModule } from "./features/features.module";
-import { MaterialModule } from "./material/material.module";
-import { StoreModule } from '@ngrx/store';
+import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FeaturesModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})],
+  imports: [BrowserModule, PagesModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
